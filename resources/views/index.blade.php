17ebaaae48dce655e69ba993c7110f2a3cfbfedf
goha <gohavorsin@gmail.com>
2017-09-14 05:42:36 +1000

    first change

---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en" class="no-js">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
+
+    <link rel="stylesheet" href="{{asset('css/reset.css')}}"> <!-- CSS reset -->
+    <link rel="stylesheet" href="{{asset('css/style.css')}}"> <!-- Resource style -->
+    <script src="{{asset('js/modernizr.js')}}"></script> <!-- Modernizr -->
+    <title>FAQ</title>
+</head>
+<body>
+    @yield('content')
+<script src="{{asset('js/jquery-2.1.1.js')}}"></script>
+<script src="{{asset('js/jquery.mobile.custom.min.js')}}"></script>
+<script src="{{asset('js/main.js')}}"></script> <!-- Resource jQuery -->
+</body>
+</html>
---
+++ resources/views/page/index.blade.php
@@ -0,0 +1,9 @@
+@extends('index')
+@section('content')
+    <header>
+        <h1>FAQ</h1>
+    </header>
+    <section class="cd-faq">
+    @include('page.sectionQuestion')
+    </section>
+@stop
